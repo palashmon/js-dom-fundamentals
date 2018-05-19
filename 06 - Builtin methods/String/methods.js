0734e95f0361bbde2c0e07bd510224337102a38f
Palash Mondal <palashm@live.in>
2018-05-19 21:03:37 +0530

    Added String includes() method

--- 06 - Builtin methods/String/methods.js
+++ 06 - Builtin methods/String/methods.js
@@ -8,3 +8,16 @@ var str = 'To be, or not to be, that is the question.';
 console.log(str.endsWith('question.')); // true
 console.log(str.endsWith('to be')); // false
 console.log(str.endsWith('to be', 19)); // true
+
+/**
+ * String.prototype.includes()
+ * Determines whether one string may be found within another string,
+ * returning true or false as appropriate.
+ */
+var str = 'To be, or not to be, that is the question.';
+
+console.log(str.includes('To be')); // true
+console.log(str.includes('question')); // true
+console.log(str.includes('nonexistent')); // false
+console.log(str.includes('To be', 1)); // false
+console.log(str.includes('TO BE')); // false
